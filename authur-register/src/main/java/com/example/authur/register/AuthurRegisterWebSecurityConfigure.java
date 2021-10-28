d42dd3cece1a419b6ba9987262f2a30b2d0f89f4
jibing.li <jibing.li@youbiai.com>
2021-10-28 11:07:53 +0800

    two

--- authur-register/src/main/java/com/example/authur/register/AuthurRegisterWebSecurityConfigure.java
+++ authur-register/src/main/java/com/example/authur/register/AuthurRegisterWebSecurityConfigure.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * @Description: Security
+ * @Description: Security验证
  * @Author: jibing.Li
  * @Date: 2021/10/27 18:15
  */
