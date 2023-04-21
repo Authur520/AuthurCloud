58bbf0318e8979c1b93a18b7fa0dea8617b5d171
lijb26 <lijb26@lenovo.com>
2023-04-21 15:05:40 +0800

    新建工程authur-server-study

---
+++ authur-server/authur-server-study/src/main/java/com/example/authurserverstudy/AuthurServerStudyApplication.java
@@ -0,0 +1,16 @@
+package com.example.authurserverstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author authur
+ */
+@SpringBootApplication
+public class AuthurServerStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthurServerStudyApplication.class, args);
+	}
+
+}
---
+++ authur-server/authur-server-study/src/test/java/com/example/authurserverstudy/AuthurServerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authurserverstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthurServerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
