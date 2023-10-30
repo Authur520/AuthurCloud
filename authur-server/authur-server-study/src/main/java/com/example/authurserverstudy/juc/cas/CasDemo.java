ae7856d9c800c455d5bb891cce3af377abe9e24e
lijb26 <lijb26@lenovo.com>
2023-10-30 17:41:25 +0800

    study cas and countdownlatch

---
+++ authur-server/authur-server-study/src/main/java/com/example/authurserverstudy/juc/cas/CasDemo.java
@@ -0,0 +1,29 @@
+package com.example.authurserverstudy.juc.cas;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author 邱润泽 bullock
+ *
+ * 什么是CAS ?? compare and swap  比较和设值
+ *
+ * 比较并交换
+ */
+public class CasDemo {
+
+    public static void main(String[] args) {
+
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        //快照的值是5 期望值是5 比较并交换 修改为2019
+        System.out.println("结论= "+atomicInteger.compareAndSet(5,2019));
+        //比较并交换
+        System.out.println("结论= "+atomicInteger.compareAndSet(5,2020));
+
+        System.out.println("结论= "+atomicInteger.getAndIncrement());
+
+
+        System.out.println("------"+atomicInteger.get()+"---------");
+
+    }
+}
